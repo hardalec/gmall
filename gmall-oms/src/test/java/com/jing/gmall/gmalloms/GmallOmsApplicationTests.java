1cd94e6e27a4572593a626ff814b17b94dc8d800
hardalec <alex_johnny@126.com>
2020-03-19 18:57:56 +0800

    搭建架构。。。

---
+++ gmall-oms/src/test/java/com/jing/gmall/gmalloms/GmallOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jing.gmall.gmalloms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallOmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
